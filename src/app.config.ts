03cdd344c3ec106a1b35864837141fe1448e0f66
FavouriteTasty <19307110112@fudan.edu.cn>
2024-08-02 17:41:54 +0800

    feat: 单页面应用

--- src/app.config.ts
+++ src/app.config.ts
@@ -1,11 +1,9 @@
 export default defineAppConfig({
-  pages: [
-    'pages/login/index'
-  ],
+  pages: ["pages/home/index"],
   window: {
-    backgroundTextStyle: 'light',
-    navigationBarBackgroundColor: '#fff',
-    navigationBarTitleText: 'WeChat',
-    navigationBarTextStyle: 'black'
-  }
-})
+    backgroundTextStyle: "light",
+    navigationBarBackgroundColor: "#fff",
+    navigationBarTitleText: "WeChat",
+    navigationBarTextStyle: "black",
+  },
+});
