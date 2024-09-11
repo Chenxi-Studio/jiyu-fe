373f08310fafddc1fc43660c981e4e7c3c00175d
FavouriteTasty <19307110112@fudan.edu.cn>
2024-09-11 20:29:43 +0800

    feat: 审批刷新 & 活动展示页

--- src/pages/approve/index.tsx
+++ src/pages/approve/index.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Collapse } from "@nutui/nutui-react-taro";
+import { Collapse, PullToRefresh } from "@nutui/nutui-react-taro";
 import { SmallCard } from "@/components/small-card";
 import { ArrowDown } from "@nutui/icons-react-taro";
 import { navigateTo } from "@/utils/navigator";
@@ -8,6 +8,7 @@ import { type ActivityEntity } from "@/types/entity/Activity.entity";
 import { $UI } from "@/store/UI";
 
 const Approve = (): JSX.Element => {
+  const refresh = $UI.use((state) => state.approveRefresh);
   const [toApproveList, setToApproveList] = useState<ActivityEntity[]>([]);
 
   const fetchToApproveList = async (): Promise<void> => {
@@ -18,6 +19,15 @@ const Approve = (): JSX.Element => {
       console.error("Error fetching to approve list:", error);
     }
   };
+
+  useEffect(() => {
+    if (refresh) {
+      void fetchToApproveList();
+      $UI.update("approve page refresh", (draft) => {
+        draft.approveRefresh = false;
+      });
+    }
+  }, [refresh]);
 
   useEffect(() => {
     void fetchToApproveList();
@@ -31,33 +41,47 @@ const Approve = (): JSX.Element => {
   };
 
   return (
-    <div className="pb-[150rpx]">
-      <Collapse defaultActiveName={["1"]} expandIcon={<ArrowDown />}>
-        <Collapse.Item title="待审核" name="1">
-          <div>
-            {toApproveList.map((item, index) => (
-              <div
-                key={`Publish-${index}`}
-                className="mt-2"
-                onClick={() => {
-                  handleOnclick(item);
-                }}
-              >
-                <SmallCard
-                  title={item.title}
-                  coverImage={item.coverImage}
-                  organizer={item.organizer}
-                  endTime={item.endTime}
-                ></SmallCard>
-              </div>
-            ))}
-          </div>
-        </Collapse.Item>
-        <Collapse.Item title="已审核" name="2">
-          <div>TODO: 已审核</div>
-        </Collapse.Item>
-      </Collapse>
-    </div>
+    <PullToRefresh
+      onRefresh={async () => {
+        await fetchToApproveList();
+      }}
+      renderIcon={(status) => {
+        return (
+          <>
+            {(status === "pulling" || status === "complete") && 1}
+            {(status === "canRelease" || status === "refreshing") && 2}
+          </>
+        );
+      }}
+    >
+      <div className="pb-[150rpx]">
+        <Collapse defaultActiveName={["1"]} expandIcon={<ArrowDown />}>
+          <Collapse.Item title="待审核" name="1">
+            <div>
+              {toApproveList.map((item, index) => (
+                <div
+                  key={`Publish-${index}`}
+                  className="mt-2"
+                  onClick={() => {
+                    handleOnclick(item);
+                  }}
+                >
+                  <SmallCard
+                    title={item.title}
+                    coverImage={item.coverImage}
+                    organizer={item.organizer}
+                    endTime={item.endTime}
+                  ></SmallCard>
+                </div>
+              ))}
+            </div>
+          </Collapse.Item>
+          <Collapse.Item title="已审核" name="2">
+            <div>TODO: 已审核</div>
+          </Collapse.Item>
+        </Collapse>
+      </div>
+    </PullToRefresh>
   );
 };
 
