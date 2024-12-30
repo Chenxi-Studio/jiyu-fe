e1c647c00aba546a226042b27e131a2d008ca3ae
favouritetasty <19307110112@fudan.edu.cn>
2024-12-30 20:23:54 +0800

    chore: 清理代码

--- src/pages/auth/index.tsx
+++ src/pages/auth/index.tsx
@@ -225,7 +225,7 @@ const Auth = (): JSX.Element => {
           UltraAdmin 测试登录
         </div>
       </div> */}
-      <div
+      {/* <div
         className={twMerge("fixed bottom-[32%] px-8 w-[calc(100%-128rpx)]")}
         onClick={() => {
           clearStore();
@@ -240,8 +240,8 @@ const Auth = (): JSX.Element => {
         >
           无登录 Detail
         </div>
-      </div>{" "}
-      */
+      </div>{" "} */}
+
       <div className="fixed bottom-[5%] px-8 w-[calc(100%-128rpx)] flex items-center justify-center">
         <Logo />
       </div>
